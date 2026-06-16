8b4b97a35e9251b3d2e7adfaceeb72670b31f0fa
Andrei Valcu <andreivalcu@gmail.com>
2026-06-16 03:03:21 +0300

    Fix Rector check for optional agent detector

--- rector.php
+++ rector.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
+use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
@@ -16,6 +17,12 @@ return static function (RectorConfig $rectorConfig): void {
 
     $rectorConfig->rules([
         InlineConstructorDefaultToPropertyRector::class,
+    ]);
+
+    $rectorConfig->skip([
+        StringClassNameToClassConstantRector::class => [
+            __DIR__.'/src/Support/AgentOutput.php',
+        ],
     ]);
 
     $rectorConfig->sets([
